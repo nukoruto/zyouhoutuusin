481f928a162a8af02f49f0ecaa2434b8f2a9625c
こーめい <103870202+nukoruto@users.noreply.github.com>
2025-10-10 17:51:22 +0900

    Allow configuring Step03 client from arguments

--- src/step03/Main.java
+++ src/step03/Main.java
@@ -15,35 +15,78 @@ import javax.swing.JFrame;
 
 /**
  * @author nakano
- *	Version: 1.00
- *	last change: Mar 2012
+ *      Version: 1.00
+ *      last change: Mar 2012
  */
 public class Main {
-	/**
-	 * メインメソッド
-	 */
-	public static void main(String[] args){
-		Connector con; //中継用クラスのオブジェクト
-		GUIPanel2 gui; //表示用クラスのオブジェクト
-		SimpleClient2 client; //通信用クラスのオブジェクト
+        /** デフォルトのホスト名 */
+        private static final String DEFAULT_HOST = "127.0.0.1";
+        /** デフォルトのポート番号 */
+        private static final int DEFAULT_PORT = 4000;
 
-		//GUIの表示
-		gui = new GUIPanel2();
-		JFrame frame = new JFrame("SampleAppletcation");
-		frame.setSize(500, 300);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(gui);
-		//パネルの再配置
-		gui.revalidate();
+        /**
+         * メインメソッド
+         */
+        public static void main(String[] args){
+                Connector con; //中継用クラスのオブジェクト
+                GUIPanel2 gui; //表示用クラスのオブジェクト
+                SimpleClient2 client; //通信用クラスのオブジェクト
 
-		//クライアントの起動
-		String[] data = {"127.0.0.1", "4000"}; //IPアドレスとポート番号【重要】
-		client = new SimpleClient2(data);
+                String[] connection = analizeCommandline(args);
 
-		//GUIとクライアントを繋げる
-		con = new Connector(gui, client);
-		gui.setConnector(con);
-		client.setConnector(con);
-	}
+                //GUIの表示
+                gui = new GUIPanel2();
+                JFrame frame = new JFrame("SampleAppletcation");
+                frame.setSize(500, 300);
+                frame.setVisible(true);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(gui);
+                //パネルの再配置
+                gui.revalidate();
+
+                //クライアントの起動
+                client = new SimpleClient2(connection);
+
+                //GUIとクライアントを繋げる
+                con = new Connector(gui, client);
+                gui.setConnector(con);
+                client.setConnector(con);
+        }
+
+        /**
+         * コマンドライン引数から接続先情報を決定する。
+         * 引数が指定されない場合はデフォルト値を用いる。
+         * @param args コマンドライン引数
+         * @return ホスト名とポート番号の文字列配列
+         */
+        private static String[] analizeCommandline(String[] args){
+                String host = DEFAULT_HOST;
+                int port = DEFAULT_PORT;
+
+                if(args.length == 0){
+                        System.out.println("Main> 引数が指定されなかったため、デフォルト設定("+host+":"+port+")を使用します。<analizeCommandline>");
+                        return new String[]{host, String.valueOf(port)};
+                }
+
+                if(args.length != 2){
+                        System.out.println("Main> 引数を0個または2つ指定してください。<analizeCommandline>");
+                        System.out.println("Main>  第一引数：サーバのホスト名(IPアドレス)<analizeCommandline>");
+                        System.out.println("Main>  第二引数：通信用のポート番号<analizeCommandline>");
+                        System.exit(1);
+                }
+
+                try{
+                        host = args[0];
+                        port = Integer.parseInt(args[1]);
+                        System.out.println("Main> 接続するサーバのホスト名は"+host+"、ポート番号は"+port+"とします。<analizeCommandline>");
+                        return new String[]{host, String.valueOf(port)};
+                }
+                catch(NumberFormatException e){
+                        System.err.println(""+e+":引数が数字ではありません。<analizeCommandline>");
+                        System.out.println("Main> ポート番号は半角数字で入力してください。<analizeCommandline>");
+                        System.exit(1);
+                }
+
+                return new String[]{host, String.valueOf(port)};
+        }
 }
